f518c55f78e131834a2521c33d2e9dc2f0f7858f
Rocky-crea <115366103+Rocky-crea@users.noreply.github.com>
2022-11-11 11:50:51 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "my-aws-buckete"
     key = "main"
     region = "us-east-2"
-    dynamodb_table = "my-dynamodb-table"
+    dynamodb_table = "mydynamodbtable"
   }
 }
